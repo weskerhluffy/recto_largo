da22dab6c2d8c93e6bfcdbda2a5410ee18d9007f
Darkly Dreaming Wesker <weskerhluffy@gmail.com>
2017-02-27 05:29:09 -0600

    pasa varios casos

--- src/recto_largo.py
+++ src/recto_largo.py
@@ -15,6 +15,9 @@ area_max = 0
 def recto_largo_saca_caca(pila_mierda):
     return pila_mierda[-1][0]
 
+def recto_largo_saca_caca_idx(pila_mierda):
+    return pila_mierda[-1][1]
+
 def recto_largo_core(numero, idx_num):
     global pila_mierda
     global area_max
@@ -23,12 +26,27 @@ def recto_largo_core(numero, idx_num):
     largo_recto_act = 0
     area_act = 0
     
+    limite_der_ass = idx_num - 1
     while(pila_mierda and numero <= recto_largo_saca_caca(pila_mierda)):
+        limite_izq_ass = -1
         mierda_sale = pila_mierda.pop()
         logger_cagada.debug("la mierda q sale %s, numact %u" % ((mierda_sale), numero))
+        if(pila_mierda):
+            limite_izq_ass = recto_largo_saca_caca_idx(pila_mierda) 
+        num_sale = mierda_sale[0]
+        logger_cagada.debug("el num q sale %u, limites %u-%u" % (num_sale, limite_izq_ass, limite_der_ass))
+        
+        tam_recto_ass = limite_der_ass - limite_izq_ass
+        
+        area_ass = tam_recto_ass * num_sale
+        
+        logger_cagada.debug("el tamm recto es %u, el area %u" % (tam_recto_ass, area_ass))
+        
+        if(area_ass > area_max):
+            area_max = area_ass
         
     if(pila_mierda):
-        limite_izq = pila_mierda[-1][1]
+        limite_izq = recto_largo_saca_caca_idx(pila_mierda)
         logger_cagada.debug("el idx izq es %u, num %u" % (limite_izq, recto_largo_saca_caca(pila_mierda)))
         
     largo_recto_act = idx_num - limite_izq
@@ -44,6 +62,28 @@ def recto_largo_core(numero, idx_num):
         area_max = area_act
         
     pila_mierda.append((numero, idx_num))
+    
+def recto_largo_calcula_a_la_mala(numeros):
+    num_numeros = len(numeros)
+    area_max_a = 0
+    
+    for i in range(0, num_numeros):
+#        logger_cagada.debug("donde estan %u" % i)
+        for j in range(i, num_numeros):
+#            logger_cagada.debug("calculando de %u a %u" % (i, j))
+            cacas = numeros[i:j + 1]
+            alt_max = min(cacas)
+            area_actu = alt_max * len(cacas)
+            if(area_actu == 342):
+                logger_cagada.debug("a la mierda %u,%u con alt max %u" % (i, j, alt_max))
+            if(area_actu > area_max_a):
+                area_max_a = area_actu
+    
+    logger_cagada.debug("area max a la brava %u" % area_max_a)
+    return area_max_a
+            
+            
+
     
 def recto_largo_main():
     lineas = list(sys.stdin)
@@ -71,8 +111,13 @@ def recto_largo_main():
         
         assert ante < num
         
-        
     logger_cagada.debug("el area max es %u" % area_max)
+    
+    if(nivel_log==logging.DEBUG):
+        shit = recto_largo_calcula_a_la_mala(numeros)
+    
+        assert shit == area_max, "la mierda debug %u, el area max %u" % (shit, area_max)
+    
     print("%u" % area_max)
         
     
